1f9ca3264a3862c44c2f0235e5961220ceab7819
Stacey23trevor <55548258+Stacey23trevor@users.noreply.github.com>
2019-10-23 10:27:14 -0600

    Add files via upload

---
+++ Chapterish 1/Exercise 1_13.java
@@ -0,0 +1,12 @@
+public class Exercise 1_13.java {
+	public static void main (String[] args) {
+		//Displays the value of x
+		System.out.println ("X is = to");
+		System.out.println (44.5 * 0.55 - 50.2 * 5.9 / 3.4 * 0.55 - 50.2 * 2.1);
+		//Displays the value of y
+		System.out.println ("Y is = to ");
+		System.out.println (3.4 * 5.9 - 44.5 * 2.1 / 3.4 * 0.55 - 50.2 * 2.1);
+	}
+}
+	
+		
